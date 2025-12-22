b7930ab5349b4c0fbe292815e6435e2ea73bf470
marcher3141592 <bianchipatrick2010@gmail.com>
2025-12-22 08:52:58 +0100

    Add draw_grid function to render grid and axes
    Implement grid drawing function with axes.

---
+++ grid.cpp
@@ -0,0 +1,22 @@
+    // --- Disegna griglia e assi ---
+void draw_grid(int w, int h, int x0, int y0){
+    for(int y=0;y<h;y++)
+    {
+        for(int x=0;x<w;x++)
+        {
+            unsigned char *p = &img[(y*w+x)*3];
+
+            // griglia fine
+            if ((x-x0)%unit==0 || (y-y0)%unit==0)
+                *p = *(p+1) = *(p+2) = 15;
+
+            // griglia principale ogni 100px
+            if ((x-x0)%(unit*10)==0 || (y-y0)%(unit*10)==0)
+                *p = *(p+1) = *(p+2) = 40;
+
+            // assi
+            if (x==x0 || y==y0)
+                *p = *(p+1) = *(p+2) = 200;
+        }
+    }
+}
